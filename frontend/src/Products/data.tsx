89e734912ac0a36021754ed24ec1403c95f0ab02
hirad121 <hirad121@yahoo.com>
2023-10-05 18:00:08 -0400

    Catalog Feature with React!

---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,22 @@
+const productsData = {
+  products: [
+    {
+      name: "Shirt",
+      imageUrl: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numberOfReview: 10,
+    },
+    {
+      name: "Shirt",
+      imageUrl: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numberOfReviews: 10,
+    },
+  ],
+};
+
+export default productsData;
