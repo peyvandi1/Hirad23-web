7a464c261ef6dbe002401504be6cf61fbb531a43
hirad121 <hirad121@yahoo.com>
2023-10-05 19:28:24 -0400

    Order Feature!

---
+++ frontend/src/Orders/data.tsx
@@ -0,0 +1,42 @@
+const orderData = {
+  orders: [
+    {
+      _id: 1,
+      createdAt: "02/20/2021",
+      totalPrice: 42.9,
+      user: {
+        name: "Taylor",
+      },
+      isPaid: true,
+      paidAt: "02/14/2021",
+      isDelivered: false,
+      deliveredDate: null,
+    },
+    {
+      _id: 2,
+      createdAt: "02/20/2021",
+      totalPrice: 42.9,
+      user: {
+        name: "Pedro",
+      },
+      isPaid: true,
+      paidAt: "02/14/2021",
+      isDelivered: false,
+      deliveredDate: null,
+    },
+    {
+      _id: 3,
+      createdAt: "02/20/2021",
+      totalPrice: 42.9,
+      user: {
+        name: "Sanjay",
+      },
+      isPaid: true,
+      paidAt: "02/14/2021",
+      isDelivered: false,
+      deliveredDate: null,
+    },
+  ],
+};
+
+export default orderData;
