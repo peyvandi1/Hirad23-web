3317122232464b0d4056143cc4eb6d6813e1690e
hirad121 <hirad121@yahoo.com>
2023-10-05 22:08:53 -0400

    Fulfillment Feature!

---
+++ frontend/src/Fulfillmetn/data.tsx
@@ -0,0 +1,36 @@
+const fulfillmentData = {
+  fulfillment: [
+    {
+      _id: 1,
+      customerName: "Alice",
+      customerEmail: "alice@email.com",
+      product: {
+        productName: "Shirt",
+        quantity: 1,
+      },
+      shippingStatus: "Shipped",
+    },
+    {
+      _id: 2,
+      customerName: "Bob",
+      customerEmail: "bob@email.com",
+      product: {
+        productName: "Shirt",
+        quantity: 2,
+      },
+      shippingStatus: "Unshipped",
+    },
+    {
+      _id: 3,
+      customerName: "Charlie",
+      customerEmail: "charlie@email.com",
+      product: {
+        productName: "Pants",
+        quantity: 1,
+      },
+      shippingStatus: "Shipped",
+    },
+  ],
+};
+
+export default fulfillmentData;
