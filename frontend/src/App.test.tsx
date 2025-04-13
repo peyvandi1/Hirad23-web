006a4f30bfd4950d94414cbca788f35589c0638b
hirad121 <hirad121@yahoo.com>
2025-04-13 08:31:44 +0330

    Implemented Tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -8,6 +8,17 @@ describe("The main app", () => {
     expect(screen.getByText("Orders")).toBeInTheDocument();
   });
 
+  it("should render the Home link", () => {
+    render(<App />);
+    // expect(screen.getByText("Home")).toBeInTheDocument();
+    expect(screen.getByRole('link', {name: "Home"})).toBeInTheDocument();
+  });
+
+  it("should render the Home header", () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { level: 2})).toBeInTheDocument();
+  });
+
   it("should render the Payment link", () => {
     render(<App />);
     expect(screen.getByText("Payment")).toBeInTheDocument();
