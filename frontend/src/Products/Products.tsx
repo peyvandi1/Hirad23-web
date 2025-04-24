54e747c73828a2e92dad05d0b2ddb9f122992c58
peyvandi1 <peyvandi.1@osu.edu>
2025-04-24 19:25:31 +0330

    Web

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,11 +1,39 @@
-import data from "./data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  brand: string;
+  price: number;
+  rating: number;
+  numberOfReviews: number;
+  imageUrl: string;
+}
+
+const emptyProducts: Product[] = [];
 
 function Products() {
+  // state hook
+  const [products, setProducts]: [Product[], (products: Product[]) => void] = useState(emptyProducts);
+
+  useEffect(() => {
+		axios.get<Product[]>("http://localhost:5250/catalog",		// match to your backend URL
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+			.then((response) => setProducts(response.data))
+			.catch((error) => console.log(error));
+		}, []);
+
   return (
     <div className="content">
       <ul className="products">
-        {data.products.map((product) => (
-          <li>
+        {products.map((product) => (
+          <li key={product.id}>
             <div className="product">
               <img
                 className="product-image"
